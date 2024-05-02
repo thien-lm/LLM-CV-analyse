f841acce3b24253358e39758c7893b05f82dc9f8
Thien <thien.lm204608@sis.hust.edu.vn>
2024-05-02 13:32:14 +0700

    update prompt

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ uploaded_file=st.file_uploader("Upload your resume(PDF)...",type=["pdf"])
 if uploaded_file is not None:
     st.write("PDF Uploaded Successfully")
 
-col1, col2, col3 = st.columns(3,gap="medium")
+col1, col2, col3, col4 = st.columns(4,gap="medium")
 
 with col1:
     submit1 = st.button("Tell Me About the Resume")
@@ -24,6 +24,9 @@ with col2:
 
 with col3:
     submit3 = st.button("Percentage match")
+
+with col4:
+    submit4= st.button("Clear cached data")
 
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
@@ -41,6 +44,10 @@ input_prompt3 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts. 
+"""
+
+input_prompt4 = """
+clear all previous data about job description and CV
 """
 
 if submit1:
@@ -75,7 +82,14 @@ elif submit3:
         st.write("Please uplaod the resume")
 
 
-
+elif submit4:
+    if uploaded_file is not None:
+        pdf_content=input_pdf_setup(uploaded_file)
+        response=get_gemini_response(input_prompt4,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
    
 
 
--- helper_func.py
+++ helper_func.py
@@ -20,11 +20,16 @@ def get_gemini_response_keywords(input,pdf_content,prompt):
     model=genai.GenerativeModel('gemini-pro-vision')
     response=model.generate_content([input,pdf_content[0],prompt])
     return json.loads(response.text[8:-4])
-    
+
+def get_gemini_response_keywords_2(input,pdf_content,prompt):
+    model=genai.GenerativeModel('gemini-pro-vision')
+    response=model.generate_content([input,pdf_content[0],prompt])
+    return json.loads(response.text)
+
 def input_pdf_setup(uploaded_file):
     if uploaded_file is not None:
         ## Convert the PDF to image
-        images=pdf2image.convert_from_bytes(uploaded_file.read())
+        images=pdf2image.convert_from_bytes(uploaded_file.read(), poppler_path=r"C:\Users\ADMIN\Downloads\Release-24.02.0-0\poppler-24.02.0\Library\bin")
 
         first_page=images[0]
 
